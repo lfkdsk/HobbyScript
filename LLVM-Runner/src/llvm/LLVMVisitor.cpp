fb4a8d0277d88fd0cdd3b58144bda615207aaec5
liufengkai <lfk_dsk@hotmail.com>
2018-08-03 09:25:20 +0800

    import llvm to project and fix cmake

---
+++ LLVM-Runner/src/llvm/LLVMVisitor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 刘丰恺 on 2018/8/3.
+//
+
+#include "LLVMVisitor.h"
---
+++ LLVM-Runner/src/llvm/LLVMVisitor.h
@@ -0,0 +1,15 @@
+//
+// Created by 刘丰恺 on 2018/8/3.
+//
+
+#ifndef LLVM_RUNNER_LLVMVISITOR_H
+#define LLVM_RUNNER_LLVMVISITOR_H
+
+#include <llvm/ADT/APFloat.h>
+
+class LLVMVisitor {
+
+};
+
+
+#endif //LLVM_RUNNER_LLVMVISITOR_H
