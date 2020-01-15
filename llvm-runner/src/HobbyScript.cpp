389961965bf9e425d3541199cbb58c94ae75d97e
liufengkai <liufengkai@bytedance.com>
2020-01-15 20:20:56 +0800

    rm useless import.

--- llvm-runner/src/HobbyScript.cpp
+++ llvm-runner/src/HobbyScript.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <rapidjson/document.h>
-#include <gen/literals/generator_literals.h>
-#include <llvm/Support/raw_ostream.h>
 #include "visitor/AstVisitor.h"
 #include "spdlog/sinks/stdout_sinks.h"
 #include "gen/generator_context.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "gtest/gtest.h"
-#include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/ManagedStatic.h>
 #include <llvm/Support/Signals.h>
-#include <llvm/ExecutionEngine/MCJIT.h>
-#include <llvm/ExecutionEngine/Interpreter.h>
-#include <llvm/ExecutionEngine/SectionMemoryManager.h>
 #include <llvm/ExecutionEngine/JITEventListener.h>
 #include <llvm/IR/AssemblyAnnotationWriter.h>
-#include <llvm/Support/DynamicLibrary.h>
 #include <llvm/Support/FormattedStream.h>
 #include <llvm/Support/SourceMgr.h>
 
